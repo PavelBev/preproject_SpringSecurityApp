58fa0eab7766a6c10e2cdf11e31e4ae7905fd277
BOneX-A <98077341+PavelBev@users.noreply.github.com>
2024-10-13 17:22:43 +0300

    commit
    first commit

---
+++ SpringSecurityAppByPavelBev/src/main/java/ru/pavelbev/springSecurityAppByPavelBev/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package ru.pavelbev.springSecurityAppByPavelBev.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String welcome() {
+        return "home";
+    }
+}
